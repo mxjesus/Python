e39aceb06fce3f5cc9f7fd627373624dd6289e56
mxjesus <90213077+mxjesus@users.noreply.github.com>
2021-09-07 03:10:41 -0500

    Eliminar

--- Fundamentos de Sistemas Embebidos/modulomusica.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os, sys
-
-def moduloled():
-    print("button 1 pressed")
-    os.system("sudo python3 /home/pi/lightshowpi/py/synchronized_lights.py --file=/home/pi/lightshowpi/music/sample/VivaLaVida.mp3")
-    
-moduloled()
--- Fundamentos de Sistemas Embebidos/settings.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-# settings.py
-
-def init():
-    global ledList, intensidadLeds
-    ledList = [0,0,0,0,0,0,0,0,0,0]
-    intensidadLeds = 1
